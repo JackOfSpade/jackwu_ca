ec7396a821e6b1039fe9973dbd3e6f534c7d1856
Jack Wu <JackWu@gmx.com>
2020-10-30 11:58:11 -0400

    443

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -2,7 +2,7 @@
 #
 #       A string of the form: 'HOST', 'HOST:PORT', 'unix:PATH'.
 #       An IP is a valid HOST.
-bind = "0.0.0.0:8000"
+bind = "0.0.0.0:443"
 
 keyfile = "private.key"
 certfile = "certificate.crt"
