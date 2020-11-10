2101ee8c67aeea4291333df5c832bdc3881e9c05
Jack Wu <JackWu@gmx.com>
2020-11-10 15:02:51 -0500

    audio tag adjust

--- text_speech/static/text_speech/index.js
+++ text_speech/static/text_speech/index.js
@@ -1,98 +1,100 @@
 $(document).ready(function() {
-  function getCookie(name) {
-      let cookieValue = null;
-      if (document.cookie && document.cookie !== '') {
-          const cookies = document.cookie.split(';');
-          for (let i = 0; i < cookies.length; i++) {
-              const cookie = cookies[i].trim();
-              // Does this cookie string begin with the name we want?
-              if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                  cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                  break;
-              }
-          }
-      }
-      return cookieValue;
-  }
+    function getCookie(name) {
+        let cookieValue = null;
+        if (document.cookie && document.cookie !== '') {
+            const cookies = document.cookie.split(';');
+            for (let i = 0; i < cookies.length; i++) {
+                const cookie = cookies[i].trim();
+                // Does this cookie string begin with the name we want?
+                if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
 
-  const csrftoken = getCookie("csrftoken");
+    const csrftoken = getCookie("csrftoken");
 
-  function csrfSafeMethod(method) {
-      // these HTTP methods do not require CSRF protection
-      return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
-  }
+    function csrfSafeMethod(method) {
+        // these HTTP methods do not require CSRF protection
+        return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+    }
 
-  $.ajaxSetup({
-      beforeSend: function(xhr, settings) {
-          if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
-              xhr.setRequestHeader("X-CSRFToken", csrftoken);
-          }
-      }
-  });
-
-  var results_table = $("#results_table")
-  var speech = $("#speech");
-  var loading = $("#loading");
-
-  loading.hide();
-  var speed = $("#speed");
-  var handle = $("#custom-handle");
-
-  speed.slider({
-      orientation: "horizontal",
-      range: "min",
-      min: 0,
-      max: 200,
-      value: 75,
-      create: function() {
-          handle.text($( this ).slider("value"));
-          },
-      slide: function( event, ui ) {
-          handle.text(ui.value);
-      }
+    $.ajaxSetup({
+        beforeSend: function(xhr, settings) {
+            if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+                xhr.setRequestHeader("X-CSRFToken", csrftoken);
+            }
+        }
     });
 
-  $("#id_voice").selectmenu();
-  
-  var audio = $("#audio")
+    var results_table = $("#results_table")
+    var speech = $("#speech");
+    var loading = $("#loading");
 
-  $("#text_speech_form").on("submit", function(event){
-    speech.hide();
-    speech.prop("disabled", true);
-    loading.show();
+    loading.hide();
+    var speed = $("#speed");
+    var handle = $("#custom-handle");
 
-    //Prevent default django post behaviour for a form submission.
-    event.preventDefault();
-
-    $.ajax({
-      url: "/text_speech/",
-      type: "POST",
-      data: {text: $("#id_text").val(),
-        voice: $("#id_voice").val(),
-        speed: speed.slider("option", "value")},
-      dataType: "json",
-      success: function (response){
-          // For testing purposes:
-          alert(response["return_text"])
-          audio.attr("src", response["return_text"]);
-
-          loading.hide();
-          speech.prop("disabled", false);
-          speech.show();
-      },
-      error: function(xhr,errmsg,err) {
-        alert("xhr: " + xhr +
-            "\n\nerrmsg: " + errmsg +
-            "\n\nerr: " + err);
-
-        loading.hide();
-        speech.prop("disabled", false);
-        speech.show();
-      }
+    speed.slider({
+        orientation: "horizontal",
+        range: "min",
+        min: 0,
+        max: 200,
+        value: 75,
+        create: function() {
+            handle.text($( this ).slider("value"));
+            },
+        slide: function( event, ui ) {
+            handle.text(ui.value);
+        }
     });
 
-    audio.load();
-    audio.play();
-  });
+    $("#id_voice").selectmenu();
+
+    var audio = $("#audio")
+    audio.hide(0)
+
+    $("#text_speech_form").on("submit", function(event){
+        speech.hide();
+        speech.prop("disabled", true);
+        loading.show();
+        audio.hide("fast")
+
+        //Prevent default django post behaviour for a form submission.
+        event.preventDefault();
+
+        $.ajax({
+            url: "/text_speech/",
+            type: "POST",
+            data: {text: $("#id_text").val(),
+                voice: $("#id_voice").val(),
+                speed: speed.slider("option", "value")},
+            dataType: "json",
+            success: function (response){
+                // For testing purposes:
+                alert(response["return_text"])
+                audio.attr("src", response["return_text"]);
+                audio.load()
+
+                loading.hide();
+                speech.prop("disabled", false);
+                speech.show();
+
+                audio.show("fast")
+                },
+            error: function(xhr,errmsg,err) {
+                alert("xhr: " + xhr +
+                    "\n\nerrmsg: " + errmsg +
+                    "\n\nerr: " + err);
+
+                loading.hide();
+                speech.prop("disabled", false);
+                speech.show();
+            }
+        });
+    });
 })
 
