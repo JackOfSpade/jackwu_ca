82033545a8f1ef7c6653415c40556accff51a969
Jack Wu <JackWu@gmx.com>
2021-09-13 19:43:45 -0400

    reactivated gunicorn, nginx ssl not working

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -6,6 +6,6 @@ bind = "0.0.0.0:443"
 
 # Comodo SSL has been integrated into nginx. Use gunicorn for manual SSL integration in the future.
 # Gunicorn has been disabled in docker + docker-compose.
-keyfile = "private.key"
-certfile = "certificate.crt"
-ca_certs = "ca_bundle.crt"
+keyfile = "/home/jacksterwu/jackwu_ca_keys_and_settings/SSL/jackwu.key"
+certfile = "/home/jacksterwu/jackwu_ca_keys_and_settings/SSL/www_jackwu_ca.crt"
+ca_certs = "/home/jacksterwu/jackwu_ca_keys_and_settings/SSL/bundle.crt"
