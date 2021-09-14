ead67774bf8e9134bfbfc51fe8d78fb645e2e52e
Jack Wu <JackWu@gmx.com>
2021-09-13 21:55:17 -0400

    back to using gunicorn for SSL

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -6,6 +6,7 @@ bind = "0.0.0.0:443"
 
 # Comodo SSL has been integrated into nginx. Use gunicorn for manual SSL integration in the future.
 # Gunicorn has been disabled in docker + docker-compose.
+# Absolute path doesn't work, must be relative path and within the project folder.
 keyfile = "jackwu.key"
 certfile = "www_jackwu_ca.crt"
 ca_certs = "bundle.crt"
