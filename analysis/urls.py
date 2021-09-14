0de5b7385bd06deed7f0e260e79b0bf99fe0d594
Jack Wu <JackWu@gmx.com>
2021-09-14 00:40:45 -0400

    covid analysis first draft of page

--- analysis/urls.py
+++ analysis/urls.py
@@ -4,6 +4,6 @@ import analysis.views as views
 app_name = "analysis"
 
 urlpatterns = [
-    urls.path(route="stock/", view=views.stock.as_view(), name="stock_" + app_name),
+    urls.path(route="covid/", view=views.covid.as_view(), name="covid_" + app_name),
     urls.path(route="crime/", view=views.crime.as_view(), name="crime_" + app_name)
 ]
--- analysis/views.py
+++ analysis/views.py
@@ -1,11 +1,10 @@
 import django.views.generic.base as base
 
-class stock(base.TemplateView):
-    template_name = "analysis/index.html"
+class covid(base.TemplateView):
+    template_name = "analysis/covid/index.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["location"]="analysis/stock/"
         return context
 
 class crime(base.TemplateView):
