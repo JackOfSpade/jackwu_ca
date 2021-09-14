9d59f55b56c4ede524d2bfdd7b9ecc38709e8ceb
Jack Wu <JackWu@gmx.com>
2021-09-13 20:08:58 -0400

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -6,6 +6,6 @@ bind = "0.0.0.0:443"
 
 # Comodo SSL has been integrated into nginx. Use gunicorn for manual SSL integration in the future.
 # Gunicorn has been disabled in docker + docker-compose.
-keyfile = "/home/jacksterwu/jackwu_ca_keys_and_settings/SSL/jackwu.key"
-certfile = "/home/jacksterwu/jackwu_ca_keys_and_settings/SSL/www_jackwu_ca.crt"
-ca_certs = "/home/jacksterwu/jackwu_ca_keys_and_settings/SSL/bundle.crt"
+keyfile = "jackwu.key"
+certfile = "www_jackwu_ca.crt"
+ca_certs = "bundle.crt"
